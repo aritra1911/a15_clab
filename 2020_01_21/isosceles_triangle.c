86456862bec22cc0ce533fe684693ed87e5b49c3
Aritra Sarkar <aritrasrkr1911@gmail.com>
2020-01-21 13:49:20 +0530

    Lab class for 21st Jan'20

---
+++ 2020_01_21/isosceles_triangle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    for (int i=0; i<5; i++) {
+        for (int k=0; k<4-i; k++)
+            printf(" ");
+        
+        for (int j=0; j<=i; j++)
+            printf("* ");
+
+        printf("\n");
+    }
+
+    return 0;
+}
