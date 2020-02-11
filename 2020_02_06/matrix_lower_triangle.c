6875234bdcb008be6d0404293a5f8a470af92ea4
Aritra Sarkar <aritrasrkr1911@gmail.com>
2020-02-11 09:34:03 +0530

    Lab class on 6th Feb'20

---
+++ 2020_02_06/matrix_lower_triangle.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], b[10][10], m, n;
+
+    do {
+        printf("Enter number of rows and columns separated by a space:\n");
+        scanf("%d %d", &m, &n);
+        if (m > 10 || n > 10)
+            printf("Please enter a dimension within 10x10.\n");
+    } while (m > 10 || n > 10);
+
+    printf("\nEnter elements:\n");
+    for (int i=0; i<m; i++) {
+        printf("Row %d\n", i);
+        for (int j=0; j<n; j++) {
+            printf("Column %d: ", j);
+            scanf("%d", &a[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++)
+            if (i < j)
+                printf("  ");
+            else
+                printf("%d ", a[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ 2020_02_06/notes.md
@@ -0,0 +1,9 @@
+Q1. WAP to find the product of two matrices.
+
+Q2. WAP to find the largest and the second largest element of a matrix.
+
+Q3. WAP to find the transpose of a matrix.
+
+Q4. WAP to print the lower triangle of a matrix.
+
+Q5. WAP to print the upper triangle of a matrix.
